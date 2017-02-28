2a22dbf4fc9da27eabfdad3bf0eded18eb2343cd
Amal <amalchirackal@gmail.com>
2017-02-28 21:27:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-
-var time = require('time');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'Hello World ',
     });
 };
